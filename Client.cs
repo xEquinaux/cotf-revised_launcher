4363b42d8606b8be852de0da12c1f1975abd98d7
Nolan Flammini <nolantheturtle@gmail.com>
2024-06-16 11:45:15 -0700

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class ChatClient
+{
+    private TcpClient _client;
+    private NetworkStream _stream;
+
+    public ChatClient(string ip, int port)
+    {
+        _client = new TcpClient(ip, port);
+        _stream = _client.GetStream();
+        Console.WriteLine("Connected to server on " + ip + ":" + port);
+    }
+
+    public void StartChat()
+    {
+        Thread thread = new Thread(() => ReceiveMessages());
+        thread.Start();
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            _stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+    private void ReceiveMessages()
+    {
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            int bytesRead = _stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0)
+            {
+                // Server disconnected
+                break;
+            }
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received: " + message);
+        }
+    }
+}
+
+public class Client
+{
+    public void Launch()
+    {
+        ChatClient client = new ChatClient("127.0.0.1", 8000);
+        client.StartChat();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace cotf_revised
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Input s or c for server or client respectively.");
+			string text = "";
+			while (text != "s" || text != "c" || text != "exit");
+			switch (text)
+			{
+				case "s":
+					new Server().Launch();
+					break;
+				case "c":
+					new Server().Launch();
+					break;
+				default:
+				case "exit":
+					break;
+			}
+		}
+	}
+}
