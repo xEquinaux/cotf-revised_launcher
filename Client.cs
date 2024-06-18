0a02bde38abdeeefc75e6d2d1c1cc17327958b87
Nolan Flammini <nolantheturtle@gmail.com>
2024-06-17 20:10:57 -0700

    [idk]

--- Client.cs
+++ Client.cs
@@ -2,11 +2,9 @@
 
 public class Client
 {
-	public Socket socket { get; set; }
 	public void Launch()
 	{
 		ChatClient client = new ChatClient("127.0.0.1", 8000);
-		socket = client.Client.Client;
 		client.StartChat();
 	}
 }
